1d6c898a8740980b6ead72037f8278b60463d14c
Aminsab Kodaganur <akodaganur@salesforce.com>
2026-02-18 14:47:21 +0530

    demo keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   salesforce: {
-    baseUrl: 'https://your-instance.my.stm.salesforce.com',
-    clientId: 'YOUR_SALESFORCE_CLIENT_ID',
-    clientSecret: 'YOUR_SALESFORCE_CLIENT_SECRET',
+    baseUrl: 'https://commssales6.dbl55.my.stm.salesforce.com',
+    clientId: '3MVG9zcHlLbiqz.p0OifwqEB_vCS1EVsEauXP2LfGD1WXOvljKVYcCGLXQQhm4uV47wPsByocFzbu2x1ZprAD',
+    clientSecret: '2445E1E1C808DF70FE7944279188E45E45DCB099CA0D530E9343CD719E141B34',
   },
 };
